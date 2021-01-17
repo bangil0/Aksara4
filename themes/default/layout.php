8538b6f9f743cc5f99ee3fe49b2eaef6a0ab7e18
Aby Dahana <abydahana@gmail.com>
2021-01-18 06:46:34 +0700

    Removing newline of title

--- themes/default/layout.php
+++ themes/default/layout.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
 <html lang="en"<?php echo (in_array(get_userdata('language'), array('arabic')) ? ' dir="rtl"' : null); ?>>
 	<head>
-		<title>
-			<?php echo truncate($template->meta->title); ?>
-		</title>
+		<title><?php echo truncate($template->meta->title); ?></title>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="msapplication-navbutton-color" content="#007bff" />
