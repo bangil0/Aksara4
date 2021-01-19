91c1a8db9025d7b096c4b4d56af062d95fb1a230
Aby Dahana <abydahana@gmail.com>
2021-01-19 13:53:14 +0700

    Add the hint of  usage

--- public/install/includes/function.php
+++ public/install/includes/function.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * Helper file
+ *
+ * @author			Aby Dahana
+ * @profile			abydahana.github.io
+ * @website			www.aksaracms.com
+ * @copyright		(c) 2021 - Aksara Laboratory
+ * @since			version 4.1.19
+ */
 require_once 'Classes.php';
 
-function phrase($phrase = null)
+if(!function_exists('phrase'))
 {
-	$classes										= new Classes();
-	
-	return $classes->phrase($phrase);
+	/**
+	 * Getting the phrase of translation
+	 */
+	function phrase($phrase = null)
+	{
+		// load the classes
+		$classes									= new Classes();
+		
+		return $classes->phrase($phrase);
+	}
 }
